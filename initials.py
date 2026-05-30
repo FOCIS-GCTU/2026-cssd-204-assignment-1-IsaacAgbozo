1ed29d99156d942f63273d9c5a8f8695dc6fdffd
IsaacAgbozo <2425403781@live.gctu.edu.gh>
2026-05-30 23:13:18 +0000

    Update initials.py
    This is my assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: IAT.py # Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Isaac Agbozo Tetteh
+# STUDENT ID: "2425403781"
+# Email:2425403781@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Isaac Agbozo Tetteh, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials in normal and large format
 
+    print()
+    print("...IAT")
+    print()
+
+    print("...IIIIIIIIII...AAAAAAAAAAAA...TTTTTTTTTTTT...")
+    print("......IIII......AAA......AAA......TTTTTT......")
+    print("......IIII......AAA......AAA......TTTTTT......")
+    print("......IIII......AAA......AAA......TTTTTT......")
+    print("......IIII......AAAAAAAAAAAA......TTTTTT......")
+    print("......IIII......AAAAAAAAAAAA......TTTTTT......")
+    print("......IIII......AAA......AAA......TTTTTT......")
+    print("......IIII...***AAA......AAA.***..TTTTTT......")
+    print("......IIII...***AAA......AAA.***..TTTTTT......")
+    print("...IIIIIIIIII...AAA......AAA......TTTTT.......")
+
+    print()
 
 
 main()
